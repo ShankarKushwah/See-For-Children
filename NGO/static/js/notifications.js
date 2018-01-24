2ba90a55a4852d19b804769f3f4f36908c77a44b
Shankar Kushwah <shankarkushwah94@gmail.com>
2018-01-24 15:16:33 +0530

    sfdfg

--- NGO/static/js/notifications.js
+++ NGO/static/js/notifications.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('#notifications').popover({html: true, content: 'Loading...', trigger: 'manual'});
+
 
   $("#notifications").click(function () {
     if ($(".popover").is(":visible")) {
@@ -10,7 +10,7 @@ $(function () {
       $.ajax({
         url: '/notifications/last/',
         beforeSend: function () {
-          $(".popover-content").html("<div style='text-align:center'><img src='/static/img/loading.gif'></div>");
+          $(".popover-content").html("<div style='text-align:center'><img src='/static/img/loading.gif' /></div>");
           $("#notifications").removeClass("new-notifications");
         },
         success: function (data) {
@@ -20,23 +20,4 @@ $(function () {
     }
     return false;
   });
-
-  function check_notifications() {
-    $.ajax({
-      url: '/notifications/check/',
-      cache: false,
-      success: function (data) {
-        if (data != "0") {
-          $("#notifications").addClass("new-notifications");
-        }
-        else {
-          $("#notifications").removeClass("new-notifications");
-        }
-      },
-      complete: function () {
-        window.setTimeout(check_notifications, 30000);
-      }
-    });
-  };
-  check_notifications();
 });
